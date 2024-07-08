28a572854e77ecf0953b54a077de0f8401f02767
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-08 15:45:08 +0530

    push: script updated

--- lesson-1/public/app.js
+++ lesson-1/public/app.js
@@ -1,16 +1,23 @@
 "use strict";
-// const anchor = document.querySelector("a");
-// if (anchor) {
-//     console.log(anchor.href);
-// }
-// anchor?.blur()
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice("mario", "work on website", 250);
+const invTwo = new Invoice("luigi", "work on website", 300);
+let invoices = [];
 const form = document.querySelector(".new-item-form");
-// console.log(form.children);
 const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    console.log(type.value, tofrom.value, details.value, amount.value);
 });
--- lesson-1/src/app.ts
+++ lesson-1/src/app.ts
@@ -1,11 +1,24 @@
-// const anchor = document.querySelector("a");
-// if (anchor) {
-//     console.log(anchor.href);
-// }
-// anchor?.blur()
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice("mario", "work on website", 250);
+const invTwo = new Invoice("luigi", "work on website", 300);
+
+let invoices: Invoice[] = []
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
-// console.log(form.children);
 
 const type = document.querySelector("#type") as HTMLSelectElement;
 const tofrom = document.querySelector("#tofrom") as HTMLInputElement;

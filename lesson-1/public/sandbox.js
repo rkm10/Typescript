4d9cb36a0c6f460458321c4df34941fc54fdcbf5
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-08 15:01:57 +0530

    Type Aliases

--- lesson-1/public/sandbox.js
+++ lesson-1/public/sandbox.js
@@ -1,2 +1,5 @@
 "use strict";
-console.log('Rajkumar');
+let greet = (user) => {
+    console.log(`${user.name} says hello!`);
+};
+console.log(greet({ name: 20, uid: "1" }));
--- lesson-1/src/sandbox.ts
+++ lesson-1/src/sandbox.ts
@@ -1,1 +1,9 @@
-console.log('Rajkumar');
+type StringOrNum = string | number; /// 
+type objWithName = { name: StringOrNum, uid: StringOrNum }
+
+
+let greet = (user: objWithName) => {
+    console.log(`${user.name} says hello!`);
+}
+
+console.log(greet({ name: 20, uid: "1" }));

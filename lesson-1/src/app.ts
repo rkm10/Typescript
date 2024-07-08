7db0cc922be6d8ae3ab5c0d78b62c69f52aa52d2
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-08 15:31:36 +0530

    push: slecting all the inputs

---
+++ lesson-1/src/app.ts
@@ -0,0 +1,24 @@
+// const anchor = document.querySelector("a");
+// if (anchor) {
+//     console.log(anchor.href);
+// }
+// anchor?.blur()
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+// console.log(form.children);
+
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+    console.log(
+        type.value,
+        tofrom.value,
+        details.value,
+        amount.value
+    );
+
+})

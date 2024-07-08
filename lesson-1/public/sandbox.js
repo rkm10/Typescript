7c4cd9eae2411a820fe7a9bfd90552c93cbc54e9
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-08 15:13:12 +0530

    added: function Signatures

--- lesson-1/public/sandbox.js
+++ lesson-1/public/sandbox.js
@@ -1,5 +1,28 @@
 "use strict";
-let greet = (user) => {
-    console.log(`${user.name} says hello!`);
+//---------------------------------Type Aliases----------------------------------//
+// type StringOrNum = string | number; /// 
+// type objWithName = { name: StringOrNum, uid: StringOrNum }
+// let greet = (user: objWithName) => {
+//     console.log(`${user.name} says hello!`);
+// }
+// console.log(greet({ name: 20, uid: "1" }));
+//---------------------------------Type Aliases----------------------------------//
+let great;
+great = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
 };
-console.log(greet({ name: 20, uid: "1" }));
+console.log(great("raj", "hello....!"));
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === "add") {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+let logDetails;
+logDetails = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
+};
+logDetails({ name: "raj", age: 20 });
--- lesson-1/src/sandbox.ts
+++ lesson-1/src/sandbox.ts
@@ -1,9 +1,42 @@
-type StringOrNum = string | number; /// 
-type objWithName = { name: StringOrNum, uid: StringOrNum }
+//---------------------------------Type Aliases----------------------------------//
+
+// type StringOrNum = string | number; /// 
+// type objWithName = { name: StringOrNum, uid: StringOrNum }
 
 
-let greet = (user: objWithName) => {
-    console.log(`${user.name} says hello!`);
+// let greet = (user: objWithName) => {
+//     console.log(`${user.name} says hello!`);
+// }
+
+// console.log(greet({ name: 20, uid: "1" }));
+
+//---------------------------------Type Aliases----------------------------------//
+
+let great: (a: string, b: string) => void;
+great = (name: string, greeting: string) => {
+    console.log(`${name} says ${greeting}`);
 }
 
-console.log(greet({ name: 20, uid: "1" }));
+console.log(great("raj", "hello....!"));
+
+
+let calc: (a: number, b: number, c: string) => number;
+calc = (numOne: number, numTwo: number, action: string) => {
+    if (action === "add") {
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo;
+    }
+}
+
+let logDetails: (obj: { name: string, age: number }) => void;
+
+type person = {
+    name: string, age: number
+}
+
+logDetails = (ninja: person) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
+}
+
+logDetails({ name: "raj", age: 20 })

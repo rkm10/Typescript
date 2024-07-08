d50bb5c2faacd2ec47b5ca48dde1edddd7f205ea
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-08 12:18:45 +0530

    Dynacmic(any) types

--- lesson-1/sandbox.ts
+++ lesson-1/sandbox.ts
@@ -1,34 +1,7 @@
-let character: string;
-let age: number;
-let isLoggedIn: boolean;
-
-age = 22;
-character = "raj";
-isLoggedIn = true;
-
-
-console.log('character', character, 'age', age, 'isLoggedIn', isLoggedIn);
-
-
-let ninjas: string[] = [];
-ninjas.push('raj');
-ninjas = ['kumar', 'malluri'];
-console.log(ninjas);
-
-
-let mixed: (string | number | boolean)[] = [];
-mixed.push('raj');
-mixed.push(1);
-mixed.push(true);
-console.log(mixed);
-
-mixed = ['kumar', 2, true];
-console.log(mixed);
-
-
-let ninjaOne: object;
-ninjaOne = { name: 'raj', age: 22 };
-let ninjaTwo: { name: string, age: number };
-ninjaTwo = { name: 'rajkumar', age: 26 };
-console.log(ninjaOne,
-    'ninjaTwo======', ninjaTwo);
+let ninjaAny: any[] = [];
+ninjaAny.push(1);
+console.log(ninjaAny);
+ninjaAny.push('raj');
+console.log(ninjaAny);
+ninjaAny.push(true);
+console.log(ninjaAny);
